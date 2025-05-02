9353d1ca0b506084b44531d68b01c200f14393f0
Arya <Arya@val.id>
2025-05-02 08:19:08 +0700

    feat: fix error rename in drug

--- model/drug.go
+++ model/drug.go
@@ -1,13 +1,13 @@
 package model
 
-type model.Drug struct {
+type Drug struct {
 	ID             string `json:"ID"`             // Unique drug ID
-	Name           string `json:"Name"`           // model.Drug name
+	Name           string `json:"Name"`           // Drug name
 	Description    string `json:"Description"`    // Description of the drug
 	Manufacturer   string `json:"Manufacturer"`   // Manufacturer name
 	BatchID        string `json:"BatchID"`        // Batch ID
 	ManufacturedAt string `json:"ManufacturedAt"` // Manufacture timestamp, e.g., "2025-04-07T10:00:00Z"
-	ExpiryDate     string `json:"ExpiryDate"`     // model.Drug expiry date
+	ExpiryDate     string `json:"ExpiryDate"`     // Drug expiry date
 	Owner          string `json:"Owner"`          // Current owner (useful in chain of custody)
 	Location       string `json:"Location"`       // physical or logical location
 	Status         string `json:"Status"`         // e.g., Manufactured, InTransit, Delivered, Expired
