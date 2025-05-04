85f893ea8f49ed5b72834e993cc5cae1b39387c2
Arya <Arya@val.id>
2025-05-04 12:58:07 +0700

    feat: transfer status

--- model/transfer.go
+++ model/transfer.go
@@ -6,4 +6,5 @@ type Transfer struct {
 	ID           string    `json:"ID"`           // Unique transfer ID
 	TransferDate time.Time `json:"TransferDate"` // Transfer date
 	ReceiveDate  time.Time `json:"ReceiveDate"`  // Receive date
+	IsAccepted   bool      `json:"isAccepted"`   // null, true, false
 }
