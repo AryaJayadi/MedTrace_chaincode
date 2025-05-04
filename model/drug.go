0471fc303302a4e43aed634ca5159622e5beb91b
Arya <Arya@val.id>
2025-05-04 12:13:58 +0700

    feat: transfer model

--- model/drug.go
+++ model/drug.go
@@ -1,11 +1,8 @@
 package model
 
-import "time"
-
 type Drug struct {
-	ID           string    `json:"ID"`           // Unique drug ID
-	BatchID      string    `json:"BatchID"`      // Reference to Batch.ID
-	OwnerID      string    `json:"OwnerID"`      // Current owner
-	TransferDate time.Time `json:"TransferDate"` // Transfer date
-	ReceiveDate  time.Time `json:"ReceiveDate"`  // Receive date
+	ID         string `json:"ID"`         // Unique drug ID
+	BatchID    string `json:"BatchID"`    // Reference to Batch.ID
+	OwnerID    string `json:"OwnerID"`    // Current owner
+	TransferID string `json:"TransferID"` // ID of the transfer transaction
 }
---
+++ model/transfer.go
@@ -0,0 +1,9 @@
+package model
+
+import "time"
+
+type Transfer struct {
+	ID           string    `json:"ID"`           // Unique transfer ID
+	TransferDate time.Time `json:"TransferDate"` // Transfer date
+	ReceiveDate  time.Time `json:"ReceiveDate"`  // Receive date
+}
