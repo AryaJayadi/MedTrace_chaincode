608005244b1203049b5d54a178bb1e954c7482a3
Arya <Arya@val.id>
2025-05-04 14:16:41 +0700

    feat: change createBatch

--- dto/create_batch.go
+++ dto/create_batch.go
@@ -5,11 +5,8 @@ import (
 )
 
 type BatchCreate struct {
-	Amount       int       `json:"Amount"`       // Amount of drugs in the batch
-	Description  string    `json:"Description"`  // Drug description
-	Manufacturer string    `json:"Manufacturer"` // Manufacturer name
-	Name         string    `json:"Name"`         // Drug name
-	ExpiryDate   time.Time `json:"ExpiryDate"`   // Drug expiry date
-	Location     string    `json:"Location"`     // Location of the drug
-	Status       string    `json:"Status"`       // Drug status
+	ID                  string    `json:"ID"`                  // Unique batch ID
+	ManufactureLocation string    `json:"ManufactureLocation"` // Manufacture location
+	ExpiryDate          time.Time `json:"ExpiryDate"`          // Expiry date for all drugs in the batch
+	DrugName            string    `json:"DrugName"`            // Drug name
 }
