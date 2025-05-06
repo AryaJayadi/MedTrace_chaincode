bd9c45d2489f55873d19e437f434a98fff4cf813
Arya <Arya@val.id>
2025-05-06 09:15:24 +0700

    feat: batch exists validation

--- chaincode/smartcontract.go
+++ chaincode/smartcontract.go
@@ -11,7 +11,6 @@ import (
 	"github.com/AryaJayadi/MedTrace_chaincode/model"
 
 	"github.com/hyperledger/fabric-chaincode-go/v2/pkg/cid"
-	"github.com/hyperledger/fabric-chaincode-go/v2/shim"
 	"github.com/hyperledger/fabric-contract-api-go/v2/contractapi"
 )
 
@@ -129,6 +128,14 @@ func (s *SmartContract) CreateBatch(ctx contractapi.TransactionContextInterface,
 		return nil, fmt.Errorf("failed to unmarshal request: %v", err)
 	}
 
+	exisst, err := s.BatchExists(ctx, batchCreate.ID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check if batch exists: %v", err)
+	}
+	if exisst {
+		return nil, fmt.Errorf("batch with ID %s already exists", batchCreate.ID)
+	}
+
 	batch := model.Batch{
 		DrugName:            batchCreate.DrugName,
 		ExpiryDate:          batchCreate.ExpiryDate,
@@ -184,76 +191,6 @@ func (s *SmartContract) GetAllOrganizations(ctx contractapi.TransactionContextIn
 	return orgs, nil
 }
 
-func (s *SmartContract) CreateBatch(ctx contractapi.TransactionContextInterface, param dto.BatchCreate) error {
-	mspID, err := ctx.GetClientIdentity().GetMSPID()
-	if err != nil {
-		return fmt.Errorf("failed to get MSP ID: %v", err)
-	}
-
-	batchID := uuid.NewString()
-
-	drugCreate := dto.DrugCreate{
-		Name:        param.Name,
-		Description: param.Description,
-		BatchID:     batchID,
-		Owner:       mspID,
-		Location:    param.Location,
-		Status:      param.Status,
-	}
-
-	var drugs []string
-	for range param.Amount {
-		drugID, err := s.CreateDrug(ctx, drugCreate)
-		if err != nil {
-			return fmt.Errorf("failed to create drug: %v", err)
-		}
-		drugs = append(drugs, drugID)
-	}
-
-	batch := model.Batch{
-		ID:             batchID,
-		Manufacturer:   param.Manufacturer,
-		ManufacturedAt: time.Now(),
-		ExpiryDate:     param.ExpiryDate,
-		Drugs:          drugs,
-	}
-
-	batchJSON, err := json.Marshal(batch)
-	if err != nil {
-		return err
-	}
-
-	key := string(model.PrefixBatch) + batchID
-	return ctx.GetStub().PutState(key, batchJSON)
-}
-
-func (s *SmartContract) CreateDrug(ctx contractapi.TransactionContextInterface, param dto.DrugCreate) (string, error) {
-	DrugID := uuid.NewString()
-
-	drug := model.Drug{
-		ID:          DrugID,
-		Name:        param.Name,
-		Description: param.Description,
-		BatchID:     param.BatchID,
-		Owner:       param.Owner,
-		Location:    param.Location,
-		Status:      param.Status,
-	}
-
-	drugJSON, err := json.Marshal(drug)
-	if err != nil {
-		return "", err
-	}
-
-	key := string(model.PrefixDrug) + DrugID
-	err = ctx.GetStub().PutState(key, drugJSON)
-	if err != nil {
-		return "", err
-	}
-
-	return DrugID, nil
-}
-
 func (s *SmartContract) BatchExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
 	batchJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
@@ -261,36 +198,4 @@ func (s *SmartContract) BatchExists(ctx contractapi.TransactionContextInterface,
 	}
 
 	return batchJSON != nil, nil
-}
-
-func (s *SmartContract) GetAllBatches(ctx contractapi.TransactionContextInterface) ([]*model.Batch, error) {
-	startKey := string(model.PrefixBatch)
-	endKey := string(model.PrefixBatch) + "~"
-	resultsIterator, err := ctx.GetStub().GetStateByRange(startKey, endKey)
-	if err != nil {
-		return nil, err
-	}
-	defer func(resultsIterator shim.StateQueryIteratorInterface) {
-		err := resultsIterator.Close()
-		if err != nil {
-			fmt.Printf("failed to close iterator: %v\n", err)
-		}
-	}(resultsIterator)
-
-	var batches []*model.Batch
-	for resultsIterator.HasNext() {
-		queryResponse, err := resultsIterator.Next()
-		if err != nil {
-			return nil, err
-		}
-
-		var batch model.Batch
-		err = json.Unmarshal(queryResponse.Value, &batch)
-		if err != nil {
-			return nil, err
-		}
-		batches = append(batches, &batch)
-	}
-
-	return batches, nil
 }
