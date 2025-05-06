e068e2fa4bf66b273fe7ee7f23c5572774475f38
Arya <Arya@val.id>
2025-05-06 08:27:26 +0700

    feat: order attribute by alphabet

--- model/batch.go
+++ model/batch.go
@@ -5,10 +5,10 @@ import (
 )
 
 type Batch struct {
+	DrugName            string    `json:"DrugName"`            // Drug name
+	ExpiryDate          time.Time `json:"ExpiryDate"`          // Expiry date for all drugs in the batch
 	ID                  string    `json:"ID"`                  // Unique batch ID
 	ManufacturerName    string    `json:"ManufacturerName"`    // Manufacturer name
 	ManufactureLocation string    `json:"ManufactureLocation"` // Manufacture timestamp
 	ProductionDate      time.Time `json:"ProductionDate"`      // Production date
-	ExpiryDate          time.Time `json:"ExpiryDate"`          // Expiry date for all drugs in the batch
-	DrugName            string    `json:"DrugName"`            // Drug name
 }
