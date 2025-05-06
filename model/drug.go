c61603cc00d38ad7f24f0ecf22b473fffb88429d
Arya <Arya@val.id>
2025-05-06 08:43:03 +0700

    feat: reorder based on alphabet

--- model/drug.go
+++ model/drug.go
@@ -1,8 +1,8 @@
 package model
 
 type Drug struct {
-	ID         string `json:"ID"`         // Unique drug ID
 	BatchID    string `json:"BatchID"`    // Reference to Batch.ID
+	ID         string `json:"ID"`         // Unique drug ID
 	OwnerID    string `json:"OwnerID"`    // Current owner
 	TransferID string `json:"TransferID"` // ID of the transfer transaction
 }
