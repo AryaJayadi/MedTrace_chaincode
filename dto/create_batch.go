aea0281a6bec0153f4de077a25e6b2c1a98a281f
Arya <Arya@val.id>
2025-05-06 08:16:15 +0700

    feat: get organization

--- dto/create_batch.go
+++ dto/create_batch.go
@@ -5,8 +5,8 @@ import (
 )
 
 type BatchCreate struct {
-	ID                  string    `json:"ID"`                  // Unique batch ID
-	ManufactureLocation string    `json:"ManufactureLocation"` // Manufacture location
-	ExpiryDate          time.Time `json:"ExpiryDate"`          // Expiry date for all drugs in the batch
-	DrugName            string    `json:"DrugName"`            // Drug name
+	Amount     int       `json:"Amount"`     // Amount of drugs in the batch
+	DrugName   string    `json:"DrugName"`   // Drug name
+	ExpiryDate time.Time `json:"ExpiryDate"` // Expiry date for all drugs in the batch
+	ID         string    `json:"ID"`         // Unique batch ID
 }
