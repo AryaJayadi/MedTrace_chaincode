3fd487f608b6c164deb61a555b05e047af563a61
= <Arya@val.id>
2025-05-14 06:46:20 +0700

    feat: migration to pointers

--- dto/create_transfer.go
+++ dto/create_transfer.go
@@ -3,8 +3,8 @@ package dto
 import "time"
 
 type CreateTransfer struct {
-	DrugsID      []string  `json:"DrugsID"`      // List of drug IDs
-	ReceiverID   string    `json:"ReceiverID"`   // Receiver ID
-	SenderID     string    `json:"SenderID"`     // Sender ID
-	TransferDate time.Time `json:"TransferDate"` // Transfer date
+	DrugsID      []*string  `json:"DrugsID"`      // List of drug IDs
+	ReceiverID   *string    `json:"ReceiverID"`   // Receiver ID
+	SenderID     *string    `json:"SenderID"`     // Sender ID
+	TransferDate *time.Time `json:"TransferDate"` // Transfer date
 }
