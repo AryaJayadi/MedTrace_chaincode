e9e81bdf991297654745a562ff5c793c488edc5c
= <Arya@val.id>
2025-05-14 07:48:06 +0700

    feat: add IsTransferred

--- model/drug.go
+++ model/drug.go
@@ -1,8 +1,9 @@
 package model
 
 type Drug struct {
-	BatchID    string `json:"BatchID"`    // Reference to Batch.ID
-	ID         string `json:"ID"`         // Unique drug ID
-	OwnerID    string `json:"OwnerID"`    // Current owner
-	TransferID string `json:"TransferID"` // ID of the transfer transaction
+	BatchID       string `json:"BatchID"`       // Reference to Batch.ID
+	ID            string `json:"ID"`            // Unique drug ID
+	IsTransferred bool   `json:"isTransferred"` // Indicates if the drug has been transferred
+	OwnerID       string `json:"OwnerID"`       // Current owner
+	TransferID    string `json:"TransferID"`    // ID of the transfer transaction
 }
